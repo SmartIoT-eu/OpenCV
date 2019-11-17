b766255c553761d5a15afef30fde0a4013a17e3a
yampi <cdryampi@gmail.com>
2019-11-17 22:19:08 +0100

    alexa styles

--- curri/settings.py
+++ curri/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'djangocms_admin_style',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
