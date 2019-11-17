30bc2a97388595dcd7008b9c431418c617506be6
yampi <cdryampi@gmail.com>
2019-11-17 22:12:53 +0100

    merged scss

--- curri/urls.py
+++ curri/urls.py
@@ -13,10 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls.static import static
+from django.urls import path
+from django.conf import settings
+from base import views as base_views
 
 urlpatterns = [
-    path('',include('base.urls')),
-    path('/admin/', admin.site.urls),
-]
+    path('',base_views.index,name="index"),
+    path('admin/', admin.site.urls),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
-
+import mimetypes
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "curri.settings")
     try:
         from django.core.management import execute_from_command_line
+        mimetypes.init()
+        mimetypes.types_map['.css'] = 'text/css'
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
